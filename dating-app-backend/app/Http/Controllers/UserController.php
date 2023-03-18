b1558fb6786250522a9a526e1d26f8836553b989
Fadi Haddad <fadi.haddad.235@gmail.com>
2023-03-18 14:54:06 +0200

    added getusers function

--- dating-app-backend/app/Http/Controllers/UserController.php
+++ dating-app-backend/app/Http/Controllers/UserController.php
@@ -4,13 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function getUsers(){
-
-        $users = app_users::all();
-
-        return response->json(['users' => $users]);
+    public function getUsers()
+    {
+        $users = DB::table('app_users')->get();
+        return response()->json([
+            'status' => 'success',
+            'users' => $users
+        ]);
     }
 }
--- dating-app-backend/routes/api.php
+++ dating-app-backend/routes/api.php
@@ -3,17 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -22,6 +13,6 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('refresh', 'refresh');
 });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["middleware" => "auth:api"], function () {
+    Route::post('/users', [UserController::class, 'getUsers']);
 });
