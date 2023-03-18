dd1b967ddf6b3826af81e2a71bde27b9ab8f9f72
Fadi Haddad <fadi.haddad.235@gmail.com>
2023-03-18 22:17:59 +0200

    Created Blocks Model

--- dating-app-backend/app/Models/Blocks.php
+++ dating-app-backend/app/Models/Blocks.php
@@ -5,15 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Like extends Model
+class Block extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'liked_user_id'];
+    protected $fillable = [
+        'user_id',
+        'blocked_user_id',
+    ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-
 }
