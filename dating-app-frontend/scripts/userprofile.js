3709cd62f8b143913001c73ae64cc0f7921c637f
Fadi Haddad <fadi.haddad.235@gmail.com>
2023-03-19 23:24:39 +0200

    Replaced Static Content In Profile Page With Content From Database

--- dating-app-frontend/scripts/userprofile.js
+++ dating-app-frontend/scripts/userprofile.js
@@ -1,11 +1,30 @@
 window.onload=function(){
+    let nameElement = document.getElementById("name");
+    let emailElement = document.getElementById("email");
+    let ageElement = document.getElementById("age");
+    let locationElement = document.getElementById("location");
+    let bioElement = document.getElementById("bio");
+    let pictureElement = document.getElementById("user_picture");
+    
     let headers= {
         'Authorization': 'Bearer ' + localStorage.getItem('token')
       }
-      console.log(headers)
+    //   console.log(headers)
 axios.post("http://localhost:8000/api/profile",null, { headers })
     .then(response => {
-        console.log(response)
+        let user = response.data.users[0];
+        let name = user.name;
+        let email = user.email;
+        let location = user.location;
+        let age = user.age;
+        let bio = user.bio;
+        let profile_picture = user.profile_picture;
+        nameElement.innerHTML = name.toUpperCase();
+        emailElement.innerHTML = email;
+        ageElement.innerHTML = age;
+        locationElement.innerHTML = location;
+        bioElement.innerHTML = bio;
+        pictureElement.src = "images/"+profile_picture;
       })
       .catch(error => {
         console.log(error);
